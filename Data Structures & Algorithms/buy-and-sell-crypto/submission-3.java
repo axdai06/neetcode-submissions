ec656579abc32b55e5bd2aa4b2053a7958e661c4
AJ Dai <daijx.0223@gmail.com>
2026-07-30 16:13:24 -0700

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // initial solution - works but is slower
+        // int b = 0, currentProfit = 0, maxProfit = 0;
+        // for (int s = 0; s < prices.length; s++) {
+        //     currentProfit = prices[s] - prices[b];
+        //     while (currentProfit < 0) {
+        //         b++;
+        //         currentProfit = prices[s] - prices[b];
+        //     }
+        //     maxProfit = Math.max(maxProfit, prices[s] - prices[b]);
+        // }
+        // return Math.max(maxProfit, 0);
+
+        // b represents buying price, s represents selling price
+        int b = 0, s = 1, max = 0;
+
+        while (s < prices.length) {
+            if (prices[b] < prices[s]) {
+                max = Math.max(max, prices[s] - prices[b]);
+            } else {
+                b = s;
+            }
+            s++;
+        }
+        return max;
+    }
+}
